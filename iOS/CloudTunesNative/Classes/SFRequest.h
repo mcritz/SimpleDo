42a5527531ccd88978e58e1903872a13ddf518bf
Michael Critz <github@micahelcritz.com>
2013-06-06 06:45:36 -0700

    added stub for SFRequest object

---
+++ iOS/CloudTunesNative/Classes/SFRequest.h
@@ -0,0 +1,13 @@
+//
+//  SFRequest.h
+//  SimpleDo
+//
+//  Created by Michael Critz on 6/6/13.
+//  Copyright (c) 2013 salesforce.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFRequest : NSObject
+
+@end
---
+++ iOS/CloudTunesNative/Classes/SFRequest.m
@@ -0,0 +1,13 @@
+//
+//  SFRequest.m
+//  SimpleDo
+//
+//  Created by Michael Critz on 6/6/13.
+//  Copyright (c) 2013 salesforce.com. All rights reserved.
+//
+
+#import "SFRequest.h"
+
+@implementation SFRequest
+
+@end
